f7732225f1ef0ff3703011b2f9ada4a7ff2b4cc5
Vladimir Roncevic <elektron.ronca@gmail.com>
2026-08-02 11:07:17 +0200

    [ats_utilities] Updated workflows and QG to be used from external repo as git submodule

--- run_quality_gates.sh
+++ run_quality_gates.sh
@@ -7,9 +7,9 @@
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
 #
 
-python3 .github/scripts/check_interfaces.py
-python3 .github/scripts/check_isp.py
-python3 .github/scripts/check_module_limits.py
-python3 .github/scripts/check_srp.py
+python3 gates/gates/interfaces_checker.py ats_utilities
+python3 gates/gates/isp_checker.py ats_utilities
+python3 gates/gates/limits_checker.py ats_utilities
+python3 gates/gates/srp_checker.py ats_utilities
 
 echo "Done"
